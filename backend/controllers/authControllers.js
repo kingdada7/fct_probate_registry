87717214f0ba40345e66b4785b813ec7a642e51f
kingdada7 <toluwadada7@gmail.com>
2026-02-23 02:12:05 +0100

    finsished the get user profile

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -100,4 +100,20 @@ const loginUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export { registerUser, loginUser };
+
+//@desc get user profile
+//@route GET /api/auth/profile
+//@access Private
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { registerUser, loginUser, getUserProfile };
